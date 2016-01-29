d6197fc1a55cf6a49872ed172e640cd6d6542780
Niv Sardi <xaiki@evilgiggle.com>
2016-01-29 05:26:50 -0300

    minor

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
 
-export default class Butter.Component extends React.Component {
-}
+let ListItem = (props) => (<li>
+    {props.children}
+</li>)
+
+let List = (props) => <div>
+    <ul>
+        {this.props.items.map((it, k) => {
+             let ItemComponent = props.itemComponent;
+             <ItemComponent {...props} key={k} >{it}</ItemComponent>
+         })}
+    </ul>
+</div>;
+
+export default List;
