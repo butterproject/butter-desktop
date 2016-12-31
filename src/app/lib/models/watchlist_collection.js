fd5a8138b996a69f2bd2941437fbb965f1537f9f
vankasteelj <vankasteelj@gmail.com>
2016-12-31 02:07:00 +0100

    optimize list.js : rewrite all the things

--- src/app/lib/models/watchlist_collection.js
+++ src/app/lib/models/watchlist_collection.js
@@ -5,11 +5,26 @@
         model: App.Model.Movie,
         initialize: function () {
             this.hasMore = false;
-        },
-        getProviders: function () {
-            return {
+            this.providers = {
                 torrents: [App.Providers.get('Watchlist')]
             };
+        },
+        fetch: function () {
+            return App.Providers.get('Watchlist').fetch().then((items) => {
+                for (var i in items.results) { //hack FIXME - #557
+                    items.results[i].providers = {
+                        torrents: [],
+                        metadata: []
+                    };
+                }
+                this.add(items.results);
+                this.state = 'loaded';
+                this.trigger('loaded', this, this.state);
+            }).catch((error) => {
+                this.state = 'error';
+                this.trigger('loaded', this, this.state);
+                console.error('WatchlistCollection.fetch()', error);
+            });
         },
         fetchMore: function () {
             return;
