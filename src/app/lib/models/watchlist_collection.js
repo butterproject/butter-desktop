8ba59932f9bde5037687a1f94efc5456ccf0f3cd
Niv Sardi <xaiki@evilgiggle.com>
2017-01-01 17:43:08 -0300

    reindent watchlist
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/app/lib/models/watchlist_collection.js
+++ src/app/lib/models/watchlist_collection.js
@@ -9,21 +9,23 @@
             };
         },
         fetch: function () {
-            return App.Providers.get('Watchlist').fetch().then((items) => {
-                let watchlistProvider =  App.Providers.get('Watchlist');
-                for (var i in items.results) { //hack FIXME - #557
-                    items.results[i].providers = {
-                        torrent: watchlistProvider
-                    };
-                }
-                this.add(items.results);
-                this.state = 'loaded';
-                this.trigger('loaded', this, this.state);
-            }).catch((error) => {
-                this.state = 'error';
-                this.trigger('error', this, this.state);
-                console.error('WatchlistCollection.fetch()', error);
-            });
+            return App.Providers.get('Watchlist')
+                .fetch()
+                .then((items) => {
+                    let watchlistProvider =  App.Providers.get('Watchlist');
+                    for (var i in items.results) { //hack FIXME - #557
+                        items.results[i].providers = {
+                            torrent: watchlistProvider
+                        };
+                    }
+                    this.add(items.results);
+                    this.state = 'loaded';
+                    this.trigger('loaded', this, this.state);
+                }).catch((error) => {
+                    this.state = 'error';
+                    this.trigger('error', this, this.state);
+                    console.error('WatchlistCollection.fetch()', error);
+                });
         },
         fetchMore: function () {
             return;
