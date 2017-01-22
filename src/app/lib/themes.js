a4dc87c495bcc5aae6da9df18f4889f0320053fe
Niv Sardi <xaiki@evilgiggle.com>
2017-01-22 11:55:14 -0300

    fixups for themes
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/app/lib/themes.js
+++ src/app/lib/themes.js
@@ -4,19 +4,24 @@
     var fs = require('fs');
 
     function get_themes() {
-        var theme_files = fs.readdirSync('./themes/');
+        var theme_files = fs.readdirSync('./src/app/themes/');
         var themes = theme_files.reduce((a, file) => {
             var r = {};
             r[file] = file.slice(0, -10).split('_').join(' ');
             return Object.assign({}, a, r);
         });
 
-        var tp_theme_files = fs.readdirSync('./themes/third_party');
-        var tp_themes = tp_theme_files.reduce((a, file) => {
-            var r = {};
-            r[file] = file.slice(0, -10).split('_').join(' ') + i18n.__('(Third Party)');
-            return Object.assign({}, a, r);
-        });
+        var tp_themes = {};
+        try {
+            var tp_theme_files = fs.readdirSync('./src/app/themes/third_party');
+            tp_themes = tp_theme_files.reduce((a, file) => {
+                var r = {};
+                r[file] = file.slice(0, -10).split('_').join(' ') + i18n.__('(Third Party)');
+                return Object.assign({}, a, r);
+            });
+        } catch(e) {
+            console.warn('Couldn\'t load third party themes');
+        }
 
         return Object.assign({}, themes, tp_themes);
     }
