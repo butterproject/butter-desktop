c4681bdb2042e3a7ce12a9ad460ea3d8468775f4
Niv Sardi <xaiki@evilgiggle.com>
2017-01-24 19:33:45 -0300

    fix themes detection
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/app/lib/themes.js
+++ src/app/lib/themes.js
@@ -6,19 +6,17 @@
     function get_themes() {
         var theme_files = fs.readdirSync('./src/app/themes/');
         var themes = theme_files.reduce((a, file) => {
-            var r = {};
-            r[file] = file.slice(0, -10).split('_').join(' ');
-            return Object.assign({}, a, r);
-        });
+            a[file] = file.slice(0, -10).split('_').join(' ');
+            return a;
+        }, {});
 
         var tp_themes = {};
         try {
             var tp_theme_files = fs.readdirSync('./src/app/themes/third_party');
             tp_themes = tp_theme_files.reduce((a, file) => {
-                var r = {};
-                r[file] = file.slice(0, -10).split('_').join(' ') + i18n.__('(Third Party)');
-                return Object.assign({}, a, r);
-            });
+                a[file] = file.slice(0, -10).split('_').join(' ') + i18n.__('(Third Party)');
+                return a;
+            }, {});
         } catch(e) {
             console.warn('Couldn\'t load third party themes');
         }
