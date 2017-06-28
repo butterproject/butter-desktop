dfbe63bfaa16db411dbe182f4e10c1cea8caf4b5
btzr-io <btzr.io@gmail.com>
2017-06-28 17:36:37 -0600

    implement navbar component

--- src/app/lib/views/settings.js
+++ src/app/lib/views/settings.js
@@ -16,15 +16,32 @@
             var container = this.el; // document.createElement('div');
 
             var props = {
-                action: {
+                navbar: {
+                    title: "Settings",
                     goBack: () => {
                         App.vent.trigger('settings:close');
                         $('.filter-bar').show();
+                    },
+                    toolbar: {
+                        search: false,
+                        buttons: [
+                            {
+                                title:"Shortcuts",
+                                icon:"keyboard",
+                                action: () => false
+                            },
+                            {
+                                title:"About",
+                                icon:"help_outline",
+                                action: () => false
+                            }
+                        ]
                     }
                 },
                 tabs: this.collection,
                 settings: AdvSettings
             };
+            
             var element = React.createElement(
                 i18nProvider, {i18n: i18n},
                 React.createElement(Settings, props, null));
