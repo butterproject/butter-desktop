bf4d72779b096b245d9ad82a70af6996135e2498
btzr-io <btzr.io@gmail.com>
2017-06-30 11:36:47 -0600

    dynamic loading for themes

--- src/app/lib/themes.js
+++ src/app/lib/themes.js
@@ -4,12 +4,22 @@
     var fs = require('fs');
     var path = require('path');
 
+    function load_themes(themes) {
+        themes.map(theme => {
+            var li = document.createElement('link');
+            li.rel = 'stylesheet';
+            li.href = path.join('../../node_modules', theme, 'index.css');
+            document.head.appendChild(li);
+        });
+    }
+
     function get_new_themes() {
         var packageJson = require(path.join(process.cwd(), 'package.json'));
 
         var themes = Object.keys(packageJson.dependencies)
             .filter((p) => (/(butter-theme-.*)/.test(p)));
-                    
+
+        load_themes(themes);
         return Object.assign({}, themes);
     }
 
