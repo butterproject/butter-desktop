84283f7c174346eb3dab95b114baf23209fc41ce
Niv Sardi <xaiki@evilgiggle.com>
2018-04-07 14:09:11 -0300

    ♥ karol ♥
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import style from './styl/style.styl';
+import {Window} from 'butter-base-components';
+import {ShowDetails} from 'butter-component-show-detail';
+import List from 'butter-component-list';
+
+let imgs = [
+    'https://farm1.staticflickr.com/751/22609797817_f2e2ff56eb_o_d.jpg',
+    'https://farm6.staticflickr.com/5800/31223777911_7b960ddaae_k_d.jpg',
+    'https://farm6.staticflickr.com/5512/31337144505_0d1b06879c_k_d.jpg'
+]
+
+let BackgroundImage = ({url, visible}) => (
+    <div className={`${style.backdrop}`}
+         style={{
+             backgroundImage: `url(${url})`,
+             opacity: visible ? 1 : 0,
+             transition: `opacity 2s`
+         }}>
+    </div>)
+
+class RotatingImages extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            idx: 0,
+            show: 0,
+            hide: -1 % props.imgs.length
+        }
+    }
+
+    componentWillMount() {
+
+        this.timer = setInterval(() => {
+            this.setState((state, props) => ({
+                hide: state.idx,
+            }))
+
+            setTimeout(() => (
+                this.setState((state, props) => ({
+                    idx: (state.idx + 1) % props.imgs.length
+                }))
+            ), this.props.switchTime*10)
+
+            setTimeout(() => (
+                this.setState((state, props) => ({
+                    show: state.idx
+                }))
+            ), this.props.switchTime*200)
+        }, this.props.switchTime*1000)
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer)
+    }
+
+    render () {
+        let {imgs, switchTime} = this.props;
+        let {idx, hide, show} = this.state;
+        let nextIdx = (idx + 1) % imgs.length;
+
+        return ([
+            <BackgroundImage key={idx} url={imgs[idx]} visible={idx !== hide} />,
+            <BackgroundImage key={nextIdx} url={imgs[nextIdx]} visible={nextIdx === show} />
+        ])
+    }
+}
+
+RotatingImages.propTypes = {
+    imgs: PropTypes.array.isRequired,
+    switchTime: PropTypes.number.isRequired    
+}
+
+RotatingImages.defaultProps = {
+    switchTime: 100
+}
+
+
+let ButterNinja = ({items}) => (
+    <div>
+        <RotatingImages imgs={imgs}/>
+        <Window>
+            <List items={items} />
+        </Window>
+        <Window>
+            <ShowDetail />
+        </Window>
+    </div>
+)
+
+export default ButterNinja;
---
+++ test/data.js
@@ -0,0 +1,52 @@
+export default {
+    items: [{
+        url:"https://www.youtube.com/watch?v=Nn7ZPUKn3mI&#038;list=PLmsK4TGRR2BF9V17BwQgXCCn4zO4vQ36_",
+        img:"http://midianinja.org/files/2017/09/0.jpg",
+        title:"Histórias generosas e subversivas de Jesus Cristo",
+        synopsis:"Henrique Vieira, pastor evangélico, teólogo, militante do PSOL, apresenta em episódios quinzenais um outro lado do cristianismo, a luta pela intolerância religiosa e as histórias generosas e subversivas de Jesus Cristo de Nazaré"},
+            {
+                url:"https://www.youtube.com/watch?v=Q1U0--YzJ8Q&#038;list=PLmsK4TGRR2BEsEKQLwFYf0S21l81M8J_d",
+                img:"http://midianinja.org/files/2017/09/maxresdefault-8.jpg",
+                title:"Putíssima Trindade",
+                synopsis:"Putissima Trindade é o encontro de Indianara Siqueira, Monique Prada e Amara Moira, três puta ativistas, de diferentes partes do Brasil, que debatem o trabalho sexual e os estigmas da profissão."},
+            {
+                url:"https://www.youtube.com/watch?v=sxwBbZcEVRc&#038;list=PLmsK4TGRR2BFblyfE3tDgoh2TIMhRUMTl",
+                img:"http://midianinja.org/files/2017/09/maxresdefault-10.jpg",
+                title:"Delírios Utópicos de Cláudio Prado",
+                synopsis:"Visionário, tropicalista, vovô psicodélico, produtor cultural, ativista da contracultura e da cultura digital. Esse é Claudio Prado, guru do século XXI que semanalmente narra histórias loucas vividas, leituras sobre a conjuntura atual e projeções de um novo mundo possível, na série Delírios Utópicos de Claudio Prado"},
+            {
+                url:"https://www.youtube.com/watch?v=9H-OJemwqDM&#038;t=12s",
+                img:"http://midianinja.org/files/2017/09/Captura-de-Tela-2017-06-25-às-16.39.31-1024x576.png",
+                title:"Ridículo Político",
+                synopsis:"Marcia Tiburi, filosofa, professora, escritora e umas das principais militantes do feminismo no país, transforma em vídeo o seu livro &#8220;Ridículo Político&#8221;, onde expõem o ridículo em nosso sistema político."},
+            {
+                url:"https://www.youtube.com/watch?v=Cd9-SgZ8-Ig&#038;list=PLmsK4TGRR2BFrDpQLUd98lXgwrfJDmfWo",
+                img:"http://midianinja.org/files/2017/09/maxresdefault-12.jpg",
+                title:"Bernardo Fala",
+                synopsis:"Youtuber do canal Bernardo Fala, produtor da Baleia, publicitário e ativista. Para ele, gordo é elogio."},
+            {
+                url:"https://www.youtube.com/watch?v=PskbU_xasBY&#038;list=PLmsK4TGRR2BEZbImTYer--eRnfbrJIUEN",
+                img:"http://midianinja.org/files/2017/09/maxresdefault-13.jpg",
+                title:"Candidate-se",
+                synopsis:"Apresentado por Ale Youssef, &#8216;Candidate-se&#8217; é nosso programa que funciona como uma radar em busca de novas lideranças políticas."},
+            {
+                url:"https://www.youtube.com/watch?v=l9Dn1iXuZcs&#038;list=PLmsK4TGRR2BFDxxGGtlq_lzYO_rF5CW_r",
+                img:"http://midianinja.org/files/2017/09/Captura-de-Tela-2017-09-21-às-14.09.31.png",
+                title:"Antonia Pellegrino",
+                synopsis:"Roteirista, escritora, diretora, produtora, ativista, curadora do blog #AgoraQueSãoElas e mãe, Antonia Pellegrino aborda em seus vídeos, de forma didática e com olhar feminino assuntos que abrangem o campo da política, economia e direitos humanos"},
+            {
+                url:"https://www.youtube.com/watch?v=I4p5kouk8DE&#038;list=PLmsK4TGRR2BFxt-JVUkZaKnHnX_DOD7DD",
+                img:"http://midianinja.org/files/2017/09/153_NINJA_2013-a-2016.jpg",
+                title:"Primavera Feminista",
+                synopsis:"As mulheres tem tomado o protagonismo da luta e se fazem sempre presente nas ruas em busca de conquistas públicas para a garantia de seus direitos. Contra a criminalização do aborto, o assédio, a violência e pela inclusão das mulheres negras, são inúmeras as agendas defendidas."},
+            {
+                url:"https://www.youtube.com/watch?v=WGSArp7Qz-g&#038;list=PLmsK4TGRR2BEXBgUcHiN19oWN8rvPnf6N",
+                img:"http://midianinja.org/files/2017/09/photo_2017-09-23_13-24-17.jpg",
+                title:"Reportagens NINJA",
+                synopsis:"A crise da água em São Paulo, o caso Amarildo, as lutas indígenas, as ocupações urbanas, são inúmeros os temas abordados pela equipe de reportagem."},
+            {
+                url:"https://www.youtube.com/watch?v=Xt3oWaL_m8I&#038;list=PLmsK4TGRR2BEWt2_ZVXIzxiSeQ4Yck4iq",
+                img:"http://midianinja.org/files/2017/09/009_26508526865_5dab7eb61c_o.jpg",
+                title:"NINJA contra o golpe",
+                synopsis:"Sempre na rua, em defesa da democracia e contra o golpe institucional por qual o país passou em 2016, a produção durante o período foi intensa, mostrando tanto as grandes marchas e mobilizações, como o acampamento pela democracia, as ocupações das sedes do Ministério da Cultura e as linguagens e setores organizados."}   ]
+}
