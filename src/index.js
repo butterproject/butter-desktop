7e873f491adf074326954e40a075c1a9308eccc6
Niv Sardi <xaiki@evilgiggle.com>
2018-05-14 08:23:12 -0300

    remove rotating images and in-browser test-boilerplate
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/index.js
+++ src/index.js
@@ -33,14 +33,7 @@ import {
     ListViewContainer,
 } from './containers';
 
-import RotatingImages from './components/images';
 import Logo from './components/logo';
-
-let imgs = [
-    'https://farm1.staticflickr.com/751/22609797817_f2e2ff56eb_o_d.jpg',
-    'https://farm6.staticflickr.com/5800/31223777911_7b960ddaae_k_d.jpg',
-    'https://farm6.staticflickr.com/5512/31337144505_0d1b06879c_k_d.jpg'
-]
 
 let debug = (e)=> {debugger}
 
@@ -54,13 +47,6 @@ let NinjaWindow = () => (
             <Redirect to='/list' />
         </Switch>
     </Window>)
-
-let ButterNinja = () => (
-    <div>
-        <RotatingImages imgs={imgs}/>
-        <NinjaWindow />
-    </div>
-)
 
 let RoutedNinja = () => (
     <Router>
