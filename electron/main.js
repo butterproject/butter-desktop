689c447aab5bbac75ee4cafe4bf5f5e7482f9940
Niv Sardi <xaiki@evilgiggle.com>
2018-05-15 05:04:53 -0300

    packager working
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- electron/main.js
+++ electron/main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 app.on('ready', () => {
     if (process.env.NODE_ENV === 'development') {
-        require('electron-reload')(`${__dirname}/build`)
+        require('electron-reload')(`${__dirname}/../build`)
 
         const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS, REACT_PERF } = require('electron-devtools-installer');
 
@@ -18,8 +18,14 @@ app.on('ready', () => {
         ))
     }
 
-    mainWindow = new BrowserWindow({width: 800, height: 600, frame: false, transparent: true, defaultEncoding: 'utf8'})
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false,
+        transparent: true,
+        defaultEncoding: 'utf8'
+    })
 
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    mainWindow.loadURL(`file://${__dirname}/../index.html`)
 
 })
