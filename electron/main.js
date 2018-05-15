198145436ef210c7acd309c4eb67b057d2202dcf
Niv Sardi <xaiki@evilgiggle.com>
2018-05-15 18:43:08 -0300

    import default electron handlers from boilerplate
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- electron/main.js
+++ electron/main.js
@@ -27,5 +27,21 @@ app.on('ready', () => {
     })
 
     mainWindow.loadURL(`file://${__dirname}/index.html`)
-
 })
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
