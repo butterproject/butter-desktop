00339e39cee720aad8e0c533c5e7d444ad675d9a
Niv Sardi <xaiki@evilgiggle.com>
2018-05-17 17:22:39 -0300

    fix iframe arguments, take 2
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/player.js
+++ src/components/player.js
@@ -6,7 +6,7 @@ const Identity = a => a
 const Player = ({id, goBack = {action: Identity, title: "Go Back"}}) => (
     <div>
         <Navbar type='player-nav' goBack={goBack}/>
-        <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=1`} frameborder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=1`} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
     </div>
 )
 
