0d74ec7b01507ed9e17a0a1924ccde86b203db1c
Niv Sardi <xaiki@evilgiggle.com>
2018-05-21 05:34:46 -0300

    export windowActions creation in utils
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,29 @@ import React from 'react';
 
 import {Toolbar} from 'butter-base-components';
 
+/* electron */
+import {remote} from 'electron'
+
+const doOnWindow = (fn) => (
+    (arg) => {
+        const window = remote.getCurrentWindow()
+
+        if (window) {
+            return fn(window, arg)
+        }
+
+        return null
+    }
+)
+
+const windowActions = {
+    close: doOnWindow(window => window.close()),
+    min: doOnWindow(window => window.minimize()),
+    max: doOnWindow(window => window.isMaximized() ? window.unmaximize() : window.maximize()),
+    fullscreen: doOnWindow((window, active) => window.setFullScreen(active))
+}
+
+
 const relativePath = (location, path) => {
     const basepath = location.pathname.split('/').slice(0, -1).join('/')
 
@@ -19,5 +42,6 @@ const defaultToolbar = (history) => (
 
 export {
     relativePath,
-    defaultToolbar
+    defaultToolbar,
+    windowActions
 }
