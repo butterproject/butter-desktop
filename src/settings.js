02401e14b7828a7bc9b3076cfda85ac4cae72f36
Niv Sardi <xaiki@evilgiggle.com>
2018-05-21 05:44:34 -0300

    oops: add settings
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ src/settings.js
@@ -0,0 +1,28 @@
+import Settings from 'butter-settings-default';
+
+Settings.tabs = {
+    movie: {
+        order: 1,
+        name: 'Movies',
+        providers: ['gdocs']
+    }, tvshow: {
+        order: 2,
+        name: 'Series',
+        providers: [
+            'youtube?channel=kurzgesagt',
+            'youtube?channel=devinsupertramp',
+            'youtube?channel=TEDtalksDirector',
+            'youtube?channel=BadLipReading',
+            'youtube?channel=1veritasium',
+            'youtube?channel=enyay', // tom scott
+            'youtube?channel=CinemaSins',
+            'youtube?channel=TomSka',
+            'youtube?channel=ExplosmEntertainment',
+            'youtube?channel=everyframeapainting',
+            'youtube?channel=willunicycleforfood', // exurb1a
+            //'youtube?channel=petercapusottotv&titleRegex=[0-9]+[aª] +Temporada',
+        ],
+    }
+}
+
+export {Settings as default}
