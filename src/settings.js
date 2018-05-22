aa2d92f6136c9ab9660ff24f3dad341b19f2bb7a
Niv Sardi <xaiki@evilgiggle.com>
2018-05-22 03:21:41 -0300

    use midianinja as tests
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/settings.js
+++ src/settings.js
@@ -7,22 +7,23 @@ Settings.tabs = {
     providers: ['gdocs']
   },
   tvshow: {
-    order: 2,
-    name: 'Series',
-    providers: [
-      'youtube?channel=kurzgesagt',
-      'youtube?channel=devinsupertramp',
-      'youtube?channel=TEDtalksDirector',
-      'youtube?channel=BadLipReading',
-      'youtube?channel=1veritasium',
-      'youtube?channel=enyay', // tom scott
-      'youtube?channel=CinemaSins',
-      'youtube?channel=TomSka',
-      'youtube?channel=ExplosmEntertainment',
-      'youtube?channel=everyframeapainting',
-      'youtube?channel=willunicycleforfood' // exurb1a
-      // 'youtube?channel=petercapusottotv&titleRegex=[0-9]+[aª] +Temporada',
-    ]
+      order: 2,
+      name: 'Series',
+      providers: [
+          'youtube?channel=midianinjafly&mode=shows&maxResults=30',
+          'youtube?channel=kurzgesagt',
+          /*   'youtube?channel=devinsupertramp',
+             'youtube?channel=TEDtalksDirector',
+             'youtube?channel=BadLipReading',
+             'youtube?channel=1veritasium',
+             'youtube?channel=enyay', // tom scott
+             'youtube?channel=CinemaSins',
+             'youtube?channel=TomSka',
+             'youtube?channel=ExplosmEntertainment',
+             'youtube?channel=everyframeapainting',
+             'youtube?channel=willunicycleforfood' // exurb1a
+             // 'youtube?channel=petercapusottotv&titleRegex=[0-9]+[aª] +Temporada', */
+      ]
   }
 }
 
