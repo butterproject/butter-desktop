a09a6558fb1759e34334fbc6ca09926ce2caa04e
Niv Sardi <xaiki@evilgiggle.com>
2018-05-23 04:25:24 -0300

    player: only load iFrame if we actually have content,
    if not, we're loading and show something that will become a spinner ... soon ?
    
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/player.js
+++ src/components/player.js
@@ -3,15 +3,17 @@ import {Navbar} from 'butter-base-components'
 
 const Identity = a => a
 
-const Player = ({id, goBack = {action: Identity, title: 'Go Back'}}) => {
+const Player = ({id, isFetching, goBack = {action: Identity, title: 'Go Back'}}) => {
   const url = `https://www.youtube.com/embed/${id}?rel=0&autoplay=1&showinfo=0`
-  console.error('playing', url)
+    isFetching || console.error('playing', url)
 
   return (
-    <div>
-      <Navbar type='player-nav' goBack={goBack} />
-      <iframe width='100%' height='100%' src={url} frameBorder='0' allow='autoplay; encrypted-media' allowFullScreen />
-    </div>
+      <div>
+          <Navbar type='player-nav' goBack={goBack} />
+          {isFetching ? <p>Loading...</p>
+           :  <iframe width='100%' height='100%' src={url} frameBorder='0' allow='autoplay; encrypted-media' allowFullScreen />
+          }
+      </div>
   )
 }
 
