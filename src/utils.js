83571dc35fa17e35b9f8cbde90aae84f373782e5
Niv Sardi <xaiki@evilgiggle.com>
2018-05-24 02:36:14 -0300

    lint
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/utils.js
+++ src/utils.js
@@ -6,141 +6,139 @@ import {connect} from 'react-redux'
 import {bindPersistActions} from './redux/persist'
 import {providerActions} from './'
 
-
 /* electron */
 import {remote} from 'electron'
 
 const Identity = (a) => a
 
 const doOnWindow = (fn) =>
-    (arg) => {
-        const window = remote.getCurrentWindow()
+  (arg) => {
+    const window = remote.getCurrentWindow()
 
-        if (window) {
-            return fn(window, arg)
-        }
-
-        return null
+    if (window) {
+      return fn(window, arg)
     }
 
+    return null
+  }
+
 const windowActions = {
-    close: doOnWindow(window => window.close()),
-    min: doOnWindow(window => window.minimize()),
-    max: doOnWindow(window => window.isMaximized() ? window.unmaximize() : window.maximize()),
-    fullscreen: doOnWindow((window, active) => window.setFullScreen(active))
+  close: doOnWindow(window => window.close()),
+  min: doOnWindow(window => window.minimize()),
+  max: doOnWindow(window => window.isMaximized() ? window.unmaximize() : window.maximize()),
+  fullscreen: doOnWindow((window, active) => window.setFullScreen(active))
 }
 
 const relativePath = (location, path) => {
-    const basepath = location.pathname.split('/').slice(0, -1).join('/')
+  const basepath = location.pathname.split('/').slice(0, -1).join('/')
 
-    path.replace(/^\//g, '')
+  path.replace(/^\//g, '')
 
-    console.error('path', `${basepath}/${path}`)
-    return `${basepath}/${path}`
+  console.error('path', `${basepath}/${path}`)
+  return `${basepath}/${path}`
 }
 
 const defaultToolbar = (history) => (
-    <Toolbar search buttons={[
-        {title: 'settings', icon: 'settings', action: () => (history.push('/settings'))}
-    ]} />
+  <Toolbar search buttons={[
+    {title: 'settings', icon: 'settings', action: () => (history.push('/settings'))}
+  ]} />
 )
 
 let airing = {}
 
-const extractItemFromState = (processExtraProps = () => {}, processItem = Identity) => (
-    (state, props) => {
-        const {collections} = state
-        const {match, history} = props
-        let item, retItem, collection, title, extraProps = {}
+const extractItemFromState = (processExtraProps = () => {}, processItem = Identity) =>
+  (state, props) => {
+    const {collections} = state
+    const {match, history} = props
+    let item, retItem, collection
+    let extraProps = {}
 
-        try {
-            collection = collections[match.params.provider]
-            item = collection.cache[match.params.id]
-            retItem = processItem(item, state, {...props, collection}) || item
+    try {
+      collection = collections[match.params.provider]
+      item = collection.cache[match.params.id]
+      retItem = processItem(item, state, {...props, collection}) || item
 
-            if (!retItem) {
-                throw new Error('Content undefined')
-            }
+      if (!retItem) {
+        throw new Error('Content undefined')
+      }
 
-            extraProps = processExtraProps(retItem, state, {...props, collection})
-        } catch (e) {
-            /*
+      extraProps = processExtraProps(retItem, state, {...props, collection})
+    } catch (e) {
+      /*
                we're in a weird state where we couldn't even get the first item ref,
                go back home
              */
-            history.push('/')
-        }
-
-        return {
-            item,
-            collection,
-            retItem,
-            ...extraProps
-        }
+      history.push('/')
     }
-)
 
-const mergePropsWithActions = (extraActions = () => {}) => (
-    ({item, retItem, collection, ...stateProps},
-     {dispatch, ...dispatchProps},
-     {match, ...ownProps}) => {
-        if (!retItem) {
-            return {...stateProps, ...dispatchProps, ...ownProps, dispatch, match}
-        }
+    return {
+      item,
+      collection,
+      retItem,
+      ...extraProps
+    }
+  }
 
-        let {isFetching} = collection
-        const {detail} = collection
-        const actions = {
-            ...bindPersistActions(dispatch),
-            ...providerActions[match.params.provider],
-        }
+const mergePropsWithActions = (extraActions = () => {}) =>
+  ({item, retItem, collection, ...stateProps},
+    {dispatch, ...dispatchProps},
+    {match, ...ownProps}) => {
+    if (!retItem) {
+      return {...stateProps, ...dispatchProps, ...ownProps, dispatch, match}
+    }
 
-        if (detail === item.id) {
-            delete(airing[item.id])
-        } else {
-            if (isFetching || airing[item.id]) {
-                console.log('already fetching')
-            } else {
-                console.error('dispatching DETAILS', item)
-                airing[item.id] = dispatch(actions.DETAIL(item.id))
-                isFetching = true
-            }
-        }
+    let {isFetching} = collection
+    const {detail} = collection
+    const actions = {
+      ...bindPersistActions(dispatch),
+      ...providerActions[match.params.provider]
+    }
 
-         const props = {
-             ...stateProps,
-             ...dispatchProps,
-             ...ownProps,
-             ...retItem,
-             isFetching,
-             match,
-             dispatch,
-             actions,
-         }
+    if (detail === item.id) {
+      delete (airing[item.id])
+    } else {
+      if (isFetching || airing[item.id]) {
+        console.log('already fetching')
+      } else {
+        console.error('dispatching DETAILS', item)
+        airing[item.id] = dispatch(actions.DETAIL(item.id))
+        isFetching = true
+      }
+    }
 
-         return {
-             ...props,
-             actions: {
-                 ...actions,
-                 ...extraActions(props)
-             }
-         }
-     }
-)
+    const props = {
+      ...stateProps,
+      ...dispatchProps,
+      ...ownProps,
+      ...retItem,
+      isFetching,
+      match,
+      dispatch,
+      actions
+    }
+
+    return {
+      ...props,
+      actions: {
+        ...actions,
+        ...extraActions(props)
+      }
+    }
+  }
 
 const connectItem = (processExtraProps, processItem, extraActions) => (
-    connect(
-        extractItemFromState(processExtraProps, processItem),
-        undefined,
-        mergePropsWithActions(extraActions)
-    )
+  connect(
+    extractItemFromState(processExtraProps, processItem),
+    undefined,
+    mergePropsWithActions(extraActions)
+  )
 )
 
 export {
-    relativePath,
-    defaultToolbar,
-    windowActions,
-    extractItemFromState,
-    mergePropsWithActions,
-    connectItem
+  relativePath,
+  defaultToolbar,
+  windowActions,
+  extractItemFromState,
+  mergePropsWithActions,
+  connectItem
 }
