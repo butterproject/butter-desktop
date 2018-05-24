166cf7d1161d4386bda37f9693832d07941fce9f
Niv Sardi <xaiki@evilgiggle.com>
2018-05-24 01:57:40 -0300

    properly map play and show actions
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/containers/details.js
+++ src/containers/details.js
@@ -12,9 +12,14 @@ const wireGoBack = (item, {tabs}, {match, history}) => ({
 })
 
 const ContentDetailContainer = connectItem(
-  wireGoBack, undefined, ({location, history}) => ({
-    play: (item) => history.push(`${location.pathname}/play`),
-    show: (item) => history.push(`${location.pathname}/e/${item.episode}`)
-  }))(ContentDetail)
+  wireGoBack, undefined, ({match, history}) => {
+    const episodePath = (item) => item.episode ? `/e/${item.episode}` : ''
+    const seasonPath = (item) => item.season ? `/e/${item.season}` : ''
+
+    return {
+      play: (item) => history.push(`${match.url}${seasonPath(item)}${episodePath(item)}/play`),
+      show: (item) => history.push(`${match.url}${seasonPath(item)}${episodePath(item)}`)
+    }
+  })(ContentDetail)
 
 export {ContentDetailContainer as default}
