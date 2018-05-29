eca273ca8c78fae6ad803687ee76118f5c21da85
Niv Sardi <xaiki@evilgiggle.com>
2018-05-29 11:58:37 -0300

    try out telesur
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/settings.js
+++ src/settings.js
@@ -9,21 +9,12 @@ Settings.tabs = {
   tvshow: {
     order: 2,
     name: 'Series',
-    providers: [
-      'youtube?channel=midianinjafly&mode=shows&maxResults=30',
-      'youtube?channel=kurzgesagt'
-      /*   'youtube?channel=devinsupertramp',
-             'youtube?channel=TEDtalksDirector',
-             'youtube?channel=BadLipReading',
-             'youtube?channel=1veritasium',
-             'youtube?channel=enyay', // tom scott
-             'youtube?channel=CinemaSins',
-             'youtube?channel=TomSka',
-             'youtube?channel=ExplosmEntertainment',
-             'youtube?channel=everyframeapainting',
-             'youtube?channel=willunicycleforfood' // exurb1a
-             // 'youtube?channel=petercapusottotv&titleRegex=[0-9]+[aª] +Temporada', */
-    ]
+    providers: ['youtube?channel=midianinjafly&mode=shows']
+  },
+  telesur: {
+      order: 3,
+      name: 'TeleSur',
+      providers: ['youtube?channel=telesurtv&mode=shows']
   }
 }
 
