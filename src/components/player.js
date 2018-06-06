92e53b73fc1e0ac99b8777f675c50f6b53c9f79a
Niv Sardi <xaiki@evilgiggle.com>
2018-06-06 17:24:08 -0300

    implement react-html5video
    controls are not showing, but it doesn't matter as we'll probably rewrite them,
    the interface is quite nice, i think we'll go with this one
    
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/player.js
+++ src/components/player.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import {Navbar} from 'butter-base-components'
-import style from './style.css'
+
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
 
 const Identity = a => a
-
-const Video = ({url}) => (
-  <video src={url} controls autoPlay="true">
-  </video>
-)
 
 const Player = ({id, isFetching, streamer, goBack = {action: Identity, title: 'Go Back'}}) => {
   const url = `https://www.youtube.com/embed/${id}?rel=0&autoplay=1&showinfo=0`
     isFetching || console.error('playing', url)
 
   return (
-      <div>
-          <Navbar type='content-nav' goBack={goBack} />
-          {isFetching ? <p>Loading...</p>
-           : streamer.loading ? <p>Caching...</p>
-           : <Video {...streamer}/>
-          }
-      </div>
+    <div>
+        <Navbar type='content-nav' goBack={goBack} />
+        {isFetching ? <p>Loading...</p>
+         : streamer.loading || ! streamer.url ? <p>Caching...</p>
+         : <Video autoPlay
+                  controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}>
+             <source src={streamer.url} />
+         </Video>
+        }
+    </div>
   )
 }
 
