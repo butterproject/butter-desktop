a210687a4301aea8f37bce1302d3adb6f952625a
Niv Sardi <xaiki@evilgiggle.com>
2018-06-08 13:53:10 -0300

    integrate our very own Video component
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/player.js
+++ src/components/player.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import {Navbar} from 'butter-base-components'
-
-import { DefaultPlayer as Video } from 'react-html5video';
-import 'react-html5video/dist/styles.css';
+import Video from 'butter-component-video';
 
 const Identity = a => a
 
-const Player = ({id, isFetching, streamer, goBack = {action: Identity, title: 'Go Back'}}) => {
+const Player = ({id, isFetching, streamer, goBack}) => {
   const url = `https://www.youtube.com/embed/${id}?rel=0&autoplay=1&showinfo=0`
     isFetching || console.error('playing', url)
 
   return (
     <div>
-        <Navbar type='content-nav' goBack={goBack} />
         {isFetching ? <p>Loading...</p>
          : streamer.loading || ! streamer.url ? <p>Caching...</p>
-         : <Video autoPlay
-                  controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}>
+         : <Video autoPlay goBack={goBack}>
              <source src={streamer.url} />
          </Video>
         }
     </div>
   )
+}
+
+Player.defaultProps = {
+  isFetching: false,
+  streamer: {loading: false},
+  goBack: {action: Identity, title: 'Go Back'}
 }
 
 export {Player as default}
