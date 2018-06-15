5e908dbe177f50ce40378d9619e959d3e9b81bcb
Niv Sardi <xaiki@evilgiggle.com>
2018-06-15 15:24:16 -0300

    port filter reduxer to use BUTTER/FILTER paths
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/redux/filters.js
+++ src/redux/filters.js
@@ -1,27 +1,32 @@
-import {createActions, handleActions} from 'redux-actions'
+import {createAction, handleActions} from 'redux-actions'
 import debounce from 'debounce'
 
-const actions = createActions({
-  SEARCH: undefined,
-  GENRE: undefined,
-  SORTER: undefined,
-  ORDER: undefined,
-})
+const SEARCH = 'BUTTER/FILTERS/SEARCH'
+const GENRE = 'BUTTER/FILTERS/GENRE'
+const SORTER = 'BUTTER/FILTERS/SORTER'
+const ORDER = 'BUTTER/FILTERS/ORDER'
+
+const actions = {
+  SEARCH: createAction(SEARCH),
+  GENRE: createAction(GENRE),
+  SORTER: createAction(SORTER),
+  ORDER: createAction(ORDER)
+}
 
 const mapToState = (key) => (state, {payload}) => Object.assign({}, state, {[key]: payload})
 
 const reducer = handleActions({
-  SEARCH: mapToState('search'),
-  GENRE: mapToState('genre'),
-  SORTER: mapToState('sorter'),
-  ORDER: mapToState('order'),
+  [SEARCH]: mapToState('search'),
+  [GENRE]: mapToState('genre'),
+  [SORTER]: mapToState('sorter'),
+  [ORDER]: mapToState('order'),
 }, {search: null, genre: null, sorter: 'trending', order: 'desc'})
 
 const bindFiltersActions = (dispatch) => ({
-  search: debounce((term) => dispatch(actions.search(term)), 250),
-  genre: (genre) => dispatch(actions.genre(genre)),
-  sorter: (sorter) => dispatch(actions.sorter(sorter)),
-  order: (order) => dispatch(actions.order(order))
+  search: debounce((term) => dispatch(actions.SEARCH(term)), 250),
+  genre: (genre) => dispatch(actions.GENRE(genre)),
+  sorter: (sorter) => dispatch(actions.SORTER(sorter)),
+  order: (order) => dispatch(actions.ORDER(order))
 })
 
 const filters = {
